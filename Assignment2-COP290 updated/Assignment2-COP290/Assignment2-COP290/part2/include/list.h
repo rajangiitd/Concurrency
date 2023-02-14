34b9297a7421c0e00f1514985b575d1f8c6c077c
Rajan <rajangiitd@gmail.com>
2023-02-14 13:38:32 +0530

    Added some code for list.h part-2

--- Assignment2-COP290 updated/Assignment2-COP290/Assignment2-COP290/part2/include/list.h
+++ Assignment2-COP290 updated/Assignment2-COP290/Assignment2-COP290/part2/include/list.h
@@ -15,4 +15,38 @@ void list_rm(struct list* l, struct listentry* e);    // Remove an item from the
 struct listentry* list_add(struct list* l, void* data);  // Add an item to the list
 struct list* list_new();  // Return an initialized list
 int is_empty(struct list* l);  // Check if list is empty or not
+
+struct list* list_new(){
+	struct list* mylist = (struct list*) malloc(sizeof(struct list));
+	mylist->head = NULL; mylist->tail = NULL;
+	return mylist;
+}
+
+int is_empty(struct list* l){
+	if(l->head == NULL ) return 1;
+	else return 0;
+}
+
+struct listentry* list_add(struct list* l, void* data){
+	struct listentry* node = (struct listentry*) malloc(sizeof(struct listentry));
+	node->data = data; node->prev = l->tail; node->next = NULL;
+
+   if (l->tail != NULL)  l->tail->next = node;
+   if (l->head == NULL)  l->head = node;
+   l->tail = node;
+
+   return node;
+}
+
+void list_rm(struct list* l, struct listentry* e){
+	if(e == NULL) return;
+
+	if (e->prev != NULL) e->prev->next = e->next;
+    if (e->next != NULL) e->next->prev = e->prev;
+	if (e == l->head) l->head = e->next;
+    if (e == l->tail) l->tail = e->prev;
+   	
+	free(e);
+	return;
+}
 #endif
