c4b4694b013e6d9a96480ad50b98a8d7fc698ce4
Rajan <rajangiitd@gmail.com>
2023-02-14 15:47:51 +0530

    Renewed list.h file

--- Assignment2-COP290 updated/Assignment2-COP290/Assignment2-COP290/part2/include/list.h
+++ Assignment2-COP290 updated/Assignment2-COP290/Assignment2-COP290/part2/include/list.h
@@ -1,8 +1,5 @@
 #ifndef LIST_H
 #define LIST_H
-
-#include <stdlib.h>
-
 struct list {
 	struct listentry* head;
 	struct listentry* tail;
@@ -18,5 +15,4 @@ void list_rm(struct list* l, struct listentry* e);    // Remove an item from the
 struct listentry* list_add(struct list* l, void* data);  // Add an item to the list
 struct list* list_new();  // Return an initialized list
 int is_empty(struct list* l);  // Check if list is empty or not
-
 #endif
